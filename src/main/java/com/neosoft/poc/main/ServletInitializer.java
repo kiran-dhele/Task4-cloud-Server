8034c864bdf7e6730e206ac97b236d335369e910
kiran-dhele <kkdhele777@gmail.com>
2021-12-10 11:57:35 +0530

    first commit

---
+++ src/main/java/com/neosoft/poc/main/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.neosoft.poc.main;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CloudServerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/neosoft/poc/main/CloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.poc.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
